d296bc27fd6fee8fcf3893077f888d29534a7226
colbym7 <cjmsportsfan3@gmail.com>
2025-10-08 13:33:43 -0600

    Update AmazonAnalysis.R

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -4,8 +4,9 @@ library(embed)
 library(vroom)
 library(ggmosaic)
 
-train <- vroom('Data//train.csv')
-test <- vroom('Data//test.csv')
+train <- vroom('C://Users//cjmsp//Desktop//Stat348//AmazonAccess//AmazonAccess//Data//train.csv') %>%
+  mutate(ACTION=factor(ACTION))
+test <- vroom('C://Users//cjmsp//Desktop//Stat348//AmazonAccess//AmazonAccess//Data//test.csv')
 
 ggplot(data = train) +
   geom_bar(aes(x = ACTION))
@@ -44,6 +45,29 @@ my_recipe <- recipe(ACTION ~ ., data = train) %>%
   step_other(all_nominal_predictors(), threshold = .001) %>%
   step_dummy(all_nominal_predictors())
   
-  
+
 prepped_recipe <- prep(my_recipe)
 baked_data <- bake(prepped_recipe, new_data = NULL)
+
+
+
+
+logregmodel <- logistic_reg() %>%
+  set_engine('glm')
+
+log_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(logregmodel) %>%
+  fit(data=train)
+
+amazon_predictions <- predict(log_workflow,
+                              new_data=test,
+                              type='prob')
+
+kaggle_submission1 <- data.frame(
+  id = test$id,
+  Action = amazon_predictions$.pred_1
+)
+vroom_write(x=kaggle_submission1, 
+            file="C:\\Users\\cjmsp\\Desktop\\Stat348\\AmazonAccess\\AmazonAccess\\Preds\\logreg_preds.csv", 
+            delim=",")
